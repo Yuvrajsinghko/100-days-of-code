47c3095d03a760f2efea7ccb6193c408ffbb90aa
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-02-16 21:50:48 +0530

    added new js file to practice jquery

---
+++ jQuery/index.js
@@ -0,0 +1,16 @@
+
+
+
+// $("button").on("mouseover",function(){
+//     $("h1").css("color","orange");
+// });
+
+// $("input").keydown(function(event){
+//     console.log(event.key);
+//     $("h1").text(event.key);
+// });
+
+$("button").on("click",function(){
+    $("h1").slideToggle();
+});
+
